af24d796919a0055ade6b35fe8cac952a523f8ab
bruno <brunoqueiroz612@gmail.com>
2023-11-01 09:51:15 -0300

    Add postTodo to controller

--- src/main/java/com/todoapi/todo/controller/TodoController.java
+++ src/main/java/com/todoapi/todo/controller/TodoController.java
@@ -4,11 +4,15 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.todoapi.todo.model.Todo;
 import com.todoapi.todo.services.TodoService;
+
+import jakarta.validation.Valid;
 
 @RestController
 @RequestMapping("/")
@@ -21,4 +25,8 @@ public class TodoController {
         return todoService.getTodos();
     }
     
+    @PostMapping("/todos")
+    Todo postTodo(@Valid @RequestBody Todo todo){
+        return todoService.postTodo(todo);
+    }
 }
